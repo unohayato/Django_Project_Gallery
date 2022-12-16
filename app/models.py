9bdc4e7735c4b112cfeb604a42159f3afd2c882d
unohayato <mamidori1p1p@gmail.com>
2022-12-16 23:48:24 +0900

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+  project = models.CharField(max_length=255, blank=False, null=False, unique=True)
+  description = models.TextField(blank=True, null=False)
+  team = models.CharField(max_length=255, blank=False, null=False)
+  pm = models.CharField(max_length=255, blank=False, null=False)
+  second_menber = models.CharField(max_length=255, blank=False, null=False)
+  third_menber = models.CharField(max_length=255, blank=False, null=False)
+  created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=False, null=False)
+  created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=False, null=False)
+  
+  def __str__(self):
+    return self.project
+  
