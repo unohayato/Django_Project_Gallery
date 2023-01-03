c41937f6dae54cf6113df7332d36d90afbe1712a
unohayato <mamidori1p1p@gmail.com>
2023-01-03 23:28:43 +0900

    modify

--- app/views.py
+++ app/views.py
@@ -13,12 +13,28 @@ class Detail(DetailView):
 class Create(CreateView):
   model = Post
   fields = ['project', 'description', 'category', 'team', 'pm', 'second_menber', 'third_menber', 'url']
+  new = '新規投稿'
+  new_btn = '投稿'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['new'] = self.new
+    context['new_btn'] = self.new_btn
+    return context
   
   success_url = '/'
   
 class Update(UpdateView):
   model = Post
   fields = ['project', 'description','category','team', 'pm', 'second_menber', 'third_menber', 'url']
+  new = '更新'
+  new_btn = '更新'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['new'] = self.new
+    context['new_btn'] = self.new_btn
+    return context
   
   success_url = '/'
   
